372fb1b53155c2efd1046331d7eff632d8df8e22
daichihasegawa0923 <daichihasegawa052@gmail.com>
2024-12-04 00:54:12 +0900

    lint error

--- src/app/room/[roomId]/_components/canvas.tsx
+++ src/app/room/[roomId]/_components/canvas.tsx
@@ -1,14 +1,14 @@
-'use client';
+'use client'
 
 interface Props {
-  id: string;
+  id: string
 }
 
-export default function Canvas({ id }: Props) {
+export default function Canvas ({ id }: Props) {
   return (
     <canvas
       id={id}
       style={{ width: '100%', height: '100%', touchAction: 'none' }}
     />
-  );
+  )
 }
--- src/game.logic/monos/main.logic.ts
+++ src/game.logic/monos/main.logic.ts
@@ -1,179 +1,179 @@
-import { MonoBehaviour } from '@/shared/game/monobehaviour';
-import { type Object3D } from 'three';
-import { GameScene } from '@/shared/game/game.scene';
-import * as THREE from 'three';
-import { Stage1 } from './stage/stage1';
-import { CannonWorld } from '@/shared/game/cannon.world';
-import { type GameEventHandlers } from '../events/event';
-import { type RigidBodyMonoBehaviour } from './base/rigid.body.monobehaviour';
-import { MonoContainer } from '@/shared/game/mono.container';
-import { Piece } from '@/game.logic/monos/player/piece';
-import { Light } from '@/game.logic/monos/base/light';
+import { MonoBehaviour } from '@/shared/game/monobehaviour'
+import { type Object3D } from 'three'
+import { GameScene } from '@/shared/game/game.scene'
+import * as THREE from 'three'
+import { Stage1 } from './stage/stage1'
+import { CannonWorld } from '@/shared/game/cannon.world'
+import { type GameEventHandlers } from '../events/event'
+import { type RigidBodyMonoBehaviour } from './base/rigid.body.monobehaviour'
+import { MonoContainer } from '@/shared/game/mono.container'
+import { Piece } from '@/game.logic/monos/player/piece'
+import { Light } from '@/game.logic/monos/base/light'
 import {
   type GameStatus,
   type GameObject,
-  type Vector3,
-} from '@/shared/game/type';
-import type IOnline from '@/shared/game/i.online';
-import { LightHemisphere } from '@/game.logic/monos/base/light.hemisphere';
+  type Vector3
+} from '@/shared/game/type'
+import type IOnline from '@/shared/game/i.online'
+import { LightHemisphere } from '@/game.logic/monos/base/light.hemisphere'
 
 export class MainLogic extends MonoBehaviour {
-  public constructor(
+  public constructor (
     roomId: string,
     memberId: string,
     objects: GameObject[],
     status: GameStatus,
     handler: GameEventHandlers
   ) {
-    super();
-    this.roomId = roomId;
-    this.memberId = memberId;
-    this.registerPrefabs();
+    super()
+    this.roomId = roomId
+    this.memberId = memberId
+    this.registerPrefabs()
     objects.forEach((obj) => {
-      MonoContainer.createInstance(obj.className, obj);
-    });
-    this.eventHandler = handler;
-    this.status = status;
-    GameScene.setBackgroundColor(0x006644, 1.0);
+      MonoContainer.createInstance(obj.className, obj)
+    })
+    this.eventHandler = handler
+    this.status = status
+    GameScene.setBackgroundColor(0x006644, 1.0)
   }
 
-  private readonly roomId: string;
-  private readonly memberId: string;
-  private status: GameStatus;
-  private activeMemberId: string | null = null;
+  private readonly roomId: string
+  private readonly memberId: string
+  private status: GameStatus
+  private activeMemberId: string | null = null
 
-  private rotateOnDirection = { vertical: 0, horizontal: 0 };
-  private readonly maxVertical = THREE.MathUtils.degToRad(60);
-  private readonly minVertical = THREE.MathUtils.degToRad(-60);
+  private rotateOnDirection = { vertical: 0, horizontal: 0 }
+  private readonly maxVertical = THREE.MathUtils.degToRad(60)
+  private readonly minVertical = THREE.MathUtils.degToRad(-60)
 
-  public getStatus() {
-    return this.status;
+  public getStatus () {
+    return this.status
   }
 
-  public updateStats(
+  public updateStats (
     status: GameStatus,
     activeMemberId: string,
     onChange: (gs: GameStatus, activeMemberId: string) => void
   ) {
-    onChange(status, activeMemberId);
-    this.status = status;
-    this.activeMemberId = activeMemberId;
+    onChange(status, activeMemberId)
+    this.status = status
+    this.activeMemberId = activeMemberId
   }
 
-  public getObject3D(): Object3D | null {
-    return null;
+  public getObject3D (): Object3D | null {
+    return null
   }
 
-  public isMyTurn() {
-    return this.memberId === this.activeMemberId;
+  public isMyTurn () {
+    return this.memberId === this.activeMemberId
   }
 
-  private readonly eventHandler: GameEventHandlers;
+  private readonly eventHandler: GameEventHandlers
 
-  override start(): void {
-    this.init();
+  override start (): void {
+    this.init()
   }
 
-  override update(): void {
-    this.setCameraPosition();
-    this.judgeTurnEnd();
+  override update (): void {
+    this.setCameraPosition()
+    this.judgeTurnEnd()
   }
 
-  public smashById(id: string, direction: Vector3) {
+  public smashById (id: string, direction: Vector3) {
     const target = GameScene.findById(id);
 
     (target as RigidBodyMonoBehaviour)
       ?.rigidBody()
-      .velocity.set(direction.x, direction.y, direction.z);
+      .velocity.set(direction.x, direction.y, direction.z)
   }
 
-  public smash(): void {
-    if (!this.isMyTurn()) return;
-    const mainCamera = GameScene.get().getMainCamera();
-    const direction = new THREE.Vector3(0, 0, 0);
-    mainCamera.getWorldDirection(direction);
-    direction.normalize();
-    const directionResult = CannonWorld.parseFrom(direction);
-    const myObj = this.getMyObject();
-    if (!myObj) return;
+  public smash (): void {
+    if (!this.isMyTurn()) return
+    const mainCamera = GameScene.get().getMainCamera()
+    const direction = new THREE.Vector3(0, 0, 0)
+    mainCamera.getWorldDirection(direction)
+    direction.normalize()
+    const directionResult = CannonWorld.parseFrom(direction)
+    const myObj = this.getMyObject()
+    if (!myObj) return
     this.eventHandler.impulse({
       name: 'impulse',
       id: myObj.getId(),
-      direction: { ...directionResult },
-    });
-    this.rotateOnDirection = { horizontal: 0, vertical: 0 };
+      direction: { ...directionResult }
+    })
+    this.rotateOnDirection = { horizontal: 0, vertical: 0 }
   }
 
-  public changeAngle(x: number, y: number) {
-    const { vertical, horizontal } = this.rotateOnDirection;
-    const newHorizontal = horizontal + x * 0.0005;
+  public changeAngle (x: number, y: number) {
+    const { vertical, horizontal } = this.rotateOnDirection
+    const newHorizontal = horizontal + x * 0.0005
     const newVertical = THREE.MathUtils.clamp(
       vertical + y * 0.0005,
       this.minVertical,
       this.maxVertical
-    );
+    )
     this.rotateOnDirection = {
       vertical: newVertical,
-      horizontal: newHorizontal,
-    };
+      horizontal: newHorizontal
+    }
     if (this.status === 'DIRECTION' && this.isMyTurn()) {
-      const mainCamera = GameScene.get().getMainCamera();
+      const mainCamera = GameScene.get().getMainCamera()
       const directionX =
         Math.cos(this.rotateOnDirection.vertical) *
-        Math.sin(this.rotateOnDirection.horizontal);
-      const directionY = Math.sin(this.rotateOnDirection.vertical);
+        Math.sin(this.rotateOnDirection.horizontal)
+      const directionY = Math.sin(this.rotateOnDirection.vertical)
       const directionZ =
         Math.cos(this.rotateOnDirection.vertical) *
-        Math.cos(this.rotateOnDirection.horizontal);
+        Math.cos(this.rotateOnDirection.horizontal)
       const lookAtPosition = mainCamera.position
         .clone()
-        .add(new THREE.Vector3(directionX, directionY, directionZ));
-      mainCamera.lookAt(lookAtPosition);
+        .add(new THREE.Vector3(directionX, directionY, directionZ))
+      mainCamera.lookAt(lookAtPosition)
     }
   }
 
-  private setCameraPosition() {
-    const mainCamera = GameScene.get().getMainCamera();
-    const p1Position = this.getMyObject()?.getObject3D()?.position;
+  private setCameraPosition () {
+    const mainCamera = GameScene.get().getMainCamera()
+    const p1Position = this.getMyObject()?.getObject3D()?.position
     if (!p1Position) {
-      mainCamera.position.set(0, 5, 0);
-      mainCamera.rotation.set(0, mainCamera.rotation.y + 0.01, 0);
-      return;
+      mainCamera.position.set(0, 5, 0)
+      mainCamera.rotation.set(0, mainCamera.rotation.y + 0.01, 0)
+      return
     }
     // 角度に応じてカメラの位置を設定
-    const distance = 5;
-    const height = 5;
+    const distance = 5
+    const height = 5
     mainCamera.position.set(
       p1Position.x + distance * Math.sin(this.rotateOnDirection.horizontal),
       p1Position.y + height,
       p1Position.z - distance * Math.cos(this.rotateOnDirection.horizontal)
-    );
+    )
     // 自分のターンでない時は、敵の駒を見る
     if (!this.isMyTurn()) {
       mainCamera.lookAt(
         GameScene.findByType(Piece)
           .find((p) => p.getMemberId() === this.activeMemberId)
           ?.getObject3D()?.position ?? new THREE.Vector3(0, 0, 0)
-      );
-      return;
+      )
+      return
     }
     // 発射位置を決める時は、駒の位置にカメラを移動する
     if (this.status === 'DIRECTION') {
-      const { x, y, z } = p1Position;
-      mainCamera.position.set(x, y, z);
-      return;
+      const { x, y, z } = p1Position
+      mainCamera.position.set(x, y, z)
+      return
     }
-    mainCamera.lookAt(p1Position);
+    mainCamera.lookAt(p1Position)
   }
 
-  private getMyObject(): RigidBodyMonoBehaviour | undefined {
+  private getMyObject (): RigidBodyMonoBehaviour | undefined {
     return GameScene.findByType(Piece).find(
       (p) => p.getMemberId() === this.memberId
-    );
+    )
   }
 
-  private calculatePieceMotionMass() {
-    const pieces = GameScene.findByType(Piece) as RigidBodyMonoBehaviour[];
+  private calculatePieceMotionMass () {
+    const pieces = GameScene.findByType(Piece) as RigidBodyMonoBehaviour[]
     return pieces
       .map(
         (piece) =>
@@ -181,89 +181,89 @@ export class MainLogic extends MonoBehaviour {
           piece.rigidBody().angularVelocity.lengthSquared()
       )
       .reduce((prev, current) => {
-        return prev + current;
-      });
+        return prev + current
+      })
   }
 
-  private isMoveSomePieces() {
-    return this.calculatePieceMotionMass() >= 0.01;
+  private isMoveSomePieces () {
+    return this.calculatePieceMotionMass() >= 0.01
   }
 
-  private judgeTurnEnd(): void {
-    if (!this.isMyTurn()) return;
-    if (this.getStatus() !== 'MOVING') return;
+  private judgeTurnEnd (): void {
+    if (!this.isMyTurn()) return
+    if (this.getStatus() !== 'MOVING') return
     if (!this.isMoveSomePieces()) {
-      this.status = 'WAITING';
+      this.status = 'WAITING'
       setTimeout(() => {
         if (this.isMoveSomePieces()) {
-          this.status = 'MOVING';
-          return;
+          this.status = 'MOVING'
+          return
         }
         this.eventHandler.turnEnd({
           name: 'turnEnd',
           roomId: this.roomId,
-          gameObjects: GameScene.allOnline(),
-        });
+          gameObjects: GameScene.allOnline()
+        })
         // 遅延させる
-      }, 1500);
+      }, 1500)
     }
   }
 
-  public goal(goalMemberId: string) {
+  public goal (goalMemberId: string) {
     this.eventHandler.goal({
       name: 'goal',
       goalMemberId,
       roomId: this.roomId,
-      gameObjects: GameScene.allOnline(),
-    });
+      gameObjects: GameScene.allOnline()
+    })
   }
 
-  private registerPrefabs() {
+  private registerPrefabs () {
     MonoContainer.registerPrefab('Stage1', (input) => {
-      const stage1 = GameScene.findById(input.id);
+      const stage1 = GameScene.findById(input.id)
       if (stage1) {
-        return stage1;
+        return stage1
       }
-      const created = new Stage1(input.id);
-      GameScene.add(created);
-      return created;
-    });
+      const created = new Stage1(input.id)
+      GameScene.add(created)
+      return created
+    })
     MonoContainer.registerPrefab('Piece', (input) => {
-      const pieces = GameScene.findByType(Piece);
-      const target = pieces.find((p) => p.getId() === input.id);
+      const pieces = GameScene.findByType(Piece)
+      const target = pieces.find((p) => p.getId() === input.id)
       if (target) {
-        return target;
+        return target
       }
       if (!input.other) {
-        throw new Error();
+        throw new Error()
       }
       if (!input.other.number) {
-        throw new Error();
+        throw new Error()
       }
       if (!input.other.memberId) {
-        throw new Error();
+        throw new Error()
       }
       const created = new Piece({
         id: input.id,
         number: input.other.number as string,
         memberId: input.other.memberId as string,
-        position: input.position,
-      });
-      GameScene.add(created);
-      return created;
-    });
+        position: input.position
+      })
+      GameScene.add(created)
+      return created
+    })
   }
 
-  public syncAll(gameObjects: GameObject[]) {
+  public syncAll (gameObjects: GameObject[]) {
     const addTarget = gameObjects.filter(
       (go) => GameScene.findById(go.id) == null
-    );
+    )
     // const removeTarget = GameScene.allOnline().filter(
     //   (local) => gameObjects.find((go) => go.id === local.id) == null
     // );
     addTarget.forEach((t) => {
-      MonoContainer.createInstance(t.className, t);
-    });
+      MonoContainer.createInstance(t.className, t)
+    })
     // removeTarget.forEach((r) => {
     //   const target = GameScene.findById(r.id);
     //   if (target) {
@@ -271,18 +271,18 @@ export class MainLogic extends MonoBehaviour {
     //   }
     // });
     GameScene.all().forEach((r) => {
-      const targetGo = gameObjects.find((go) => go.id === r.getId());
-      if (!targetGo) return;
-      const iOnline = r as unknown as IOnline;
-      iOnline.syncFromOnline(targetGo);
-    });
+      const targetGo = gameObjects.find((go) => go.id === r.getId())
+      if (!targetGo) return
+      const iOnline = r as unknown as IOnline
+      iOnline.syncFromOnline(targetGo)
+    })
   }
 
-  private readonly light = new Light();
-  private readonly lightHemisphere = new LightHemisphere();
+  private readonly light = new Light()
+  private readonly lightHemisphere = new LightHemisphere()
 
-  private init() {
-    GameScene.add(this.light);
-    GameScene.add(this.lightHemisphere);
+  private init () {
+    GameScene.add(this.light)
+    GameScene.add(this.lightHemisphere)
   }
 }
